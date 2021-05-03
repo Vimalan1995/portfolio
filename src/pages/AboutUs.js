9c5c74d81cfe1565ad68132c54b0dcc6b668caf5
Vimalan <vimalan.karuna@gmail.com>
2021-05-03 12:49:02 -0400

    add FAQ section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from "react";
 //page components
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 //if you dont want to use div use <> instead
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
-      <ServicesSection />;
+      <ServicesSection />
+      <FaqSection />
     </>
   );
 };
